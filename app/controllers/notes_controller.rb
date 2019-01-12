9754c50987bb15154e9f10d9e72575f1c95d0686
piotrpolec <piotr.jan.polec@gmail.com>
2019-01-12 10:58:58 +0100

    update + destroy

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,6 +23,19 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
   end
 
+  def update
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+
+    redirect_to notes_path
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path
+  end
+
   private
 
   def note_params
