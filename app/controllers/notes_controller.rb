629c3a34a647ab083567b56d4964292da94cafff
piotrpolec <piotr.jan.polec@gmail.com>
2019-01-12 10:23:53 +0100

    create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,5 +8,17 @@ class NotesController < ApplicationController
     @note = Note.new
   end
 
+  def create
+    @note = Note.new(note_params)
+    @note.save
 
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:name, :content)
+  end
+  
 end
